59f0620c857143331d777a03981fe3fa8a8024cf
Shellpecker <grund.johannes+shellpecker@protonmail.com>
2025-04-29 18:04:35 +0200

    Update Install-DotNet.ps1

--- Install-DotNet.ps1
+++ Install-DotNet.ps1
@@ -1,3 +1,3 @@
 $netfxInstaller = "$env:TEMP\ndp48-x86-x64-allos-enu.exe"
-Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=840938" -OutFile $netfxInstaller
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2088631" -OutFile $netfxInstaller
 Start-Process -FilePath $netfxInstaller -ArgumentList "/quiet", "/norestart" -Wait
